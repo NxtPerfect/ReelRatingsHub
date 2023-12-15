3b7e98b539cf8e882bbf136fb943c55099a8e13e
NxtPerfect <alakaxan@protonmail.com>
2023-12-15 11:28:24 +0100

    Working on signup/login system

--- src/index.php
+++ src/index.php
@@ -9,7 +9,23 @@
 
 <body>
   <h1>Reel Ratings Hub</h1>
-  <nav />
+  <nav>
+    <div class="search">Wyszukaj Tytuł</div>
+    <div class="filter">Filtry</div>
+    <form method="POST" action="index.php">
+      <input type="submit" name="login" value="Zaloguj">
+      <input type="submit" name="register" value="Dołącz">
+    </form>
+  </nav>
+  <div class="movies">Filmy</div>
+  <?php
+  if (isset($_POST['login'])) {
+    echo "Login";
+  }
+  if (isset($_POST['register'])) {
+    echo ('<form method="POST"> <input type="text" name="name"><input type="password" name="password">');
+  }
+  ?>
 </body>
 
 </html>
