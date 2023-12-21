00fb766a63b4966daa6a2df93089cd3a8e250e44
NxtPerfect <alakaxan@protonmail.com>
2023-12-21 11:29:24 +0100

    Added login/register page, added movies grid, filters, search box, changed nav

--- src/index.php
+++ src/index.php
@@ -5,27 +5,47 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
-  <h1>Reel Ratings Hub</h1>
   <nav>
-    <div class="search">Wyszukaj Tytuł</div>
-    <div class="filter">Filtry</div>
-    <form method="POST" action="index.php">
-      <input type="submit" name="login" value="Zaloguj">
-      <input type="submit" name="register" value="Dołącz">
+    <a href="index.php" style="text-decoration: none; color: black;"><h1>Reel Ratings Hub</h1></a>
+    <form method="GET" action="index.php">
+      <div class="search">
+        <input type="text" name="title">
+          <div type="submit" name="search" value="search" class="search_icon" style="cursor: pointer;">
+            <i class="fa fa-search"></i>
+          </div>
+      </div>
     </form>
+    <div class="login">
+      <form class="login_form" method="POST" action="register.php">
+        <button type="submit" name="login" value="login" style="cursor: pointer;">Zaloguj</button>
+        <button type="submit" name="register" value="register" style="cursor: pointer;">Zarejestruj</button>
+      </form>
+    </div>
   </nav>
-  <div class="movies">Filmy</div>
-  <?php
-  if (isset($_POST['login'])) {
-    echo "Login";
-  }
-  if (isset($_POST['register'])) {
-    echo ('<form method="POST"> <input type="text" name="name"><input type="password" name="password">');
-  }
-  ?>
+  <main>
+    <div class="filter">
+      <form method="GET" action="index.php">
+        Filtry
+        <input type="checkbox" name="filter" value="action" style="cursor: pointer;">Akcji</button>
+        <input type="checkbox" name="filter" value="adventure" style="cursor: pointer;">Przygodowe</button>
+        <button type="submit" style="cursor: pointer;">Filtruj</button>
+        <button name="reset" value="reset" style="cursor: pointer;">Wyczyść filtry</button>
+    </div>
+    <div class="movies">
+      <?php
+      echo "<img src=assets/test.jpg/>";
+      echo "<img src=assets/test.jpg/>";
+      echo "<img src=assets/test.jpg/>";
+      echo "<img src=assets/test.jpg/>";
+      echo "<img src=assets/test.jpg/>";
+      echo "<img src=assets/test.jpg/>";
+      ?>
+    </div>
+  </main>
 </body>
 
 </html>
