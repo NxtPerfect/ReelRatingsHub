225767ab9d5836321a4dcb9e618607875ac6f1e2
NxtPerfect <alakaxan@protonmail.com>
2023-12-29 11:01:53 +0100

    Shows movies from database on main page, working on reviews

--- src/index.php
+++ src/index.php
@@ -1,3 +1,16 @@
+<?php
+// read filters from database
+$databasename = '127.0.0.1';
+$username = 'root';
+$password = '';
+$conn = new mysqli($databasename, $username, $password, 'ReelRatingsHub');
+
+if (!$conn) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
+
+
 <html lang="en">
 
 <head>
@@ -32,27 +45,39 @@
       <form method="GET" action="index.php">
         <h3>Filtry</h3>
         <?php
-        // read filters from database
-        $filters = array('action' => "Akcji", 'adventure' => "Przygodowe");
-        foreach ($filters as $key => $value) {
-          echo "<button class='filter_element_button' id=action type=submit name=filter value=$key style='cursor: pointer;'>$value</button>";
-        }
-        ?>
+        $query = 'SELECT DISTINCT genres FROM movies ORDER BY genres';
+        $res = mysqli_query($conn, $query);
+        $filters = mysqli_fetch_all($res, MYSQLI_ASSOC);
+        mysqli_free_result($res);
+        foreach ($filters as $filter) {
+          foreach ($filter as $f) { ?>
+            <button class='filter_element_button' id='action' type='submit' name='filter' value=<?php echo $f ?> style='cursor: pointer;'><?php echo ucwords($f) ?></button>
+        <?php }
+        } ?>
         <button class="filter_element_reset" name="reset" value="reset" style="cursor: pointer;">Wyczyść filtry</button>
       </form>
     </div>
     <div class="movies">
       <?php
-      // foreach item in database -> render div
-      $url = 'assets/test.jpg';
-      echo "<div class=image style='background-image: url($url);'><div class=image_info><h3>Top Gun</h3><p>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</p><form method=GET action=movie.php><button class=review_button type=submit name=title value='Top Gun'>Review</button></form></div></div>";
-      echo "<div class=image style='background-image: url($url);'><div class=image_info><h3>Top Gun</h3><p>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</p><form method=GET action=movie.php><button class=review_button type=submit name=title value='Top Gun'>Review</button></form></div></div>";
-      echo "<div class=image style='background-image: url($url);'><div class=image_info><h3>Top Gun</h3><p>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</p><form method=GET action=movie.php><button class=review_button type=submit name=title value='Top Gun'>Review</button></form></div></div>";
-      echo "<div class=image style='background-image: url($url);'><div class=image_info><h3>Top Gun</h3><p>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</p><form method=GET action=movie.php><button class=review_button type=submit name=title value='Top Gun'>Review</button></form></div></div>";
-      echo "<div class=image style='background-image: url($url);'><div class=image_info><h3>Top Gun</h3><p>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</p><form method=GET action=movie.php><button class=review_button type=submit name=title value='Top Gun'>Review</button></form></div></div>";
-      echo "<div class=image style='background-image: url($url);'><div class=image_info><h3>Top Gun</h3><p>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</p><form method=GET action=movie.php><button class=review_button type=submit name=title value='Top Gun'>Review</button></form></div></div>";
-      echo "<div class=image style='background-image: url($url);'><div class=image_info><h3>Top Gun</h3><p>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</p><form method=GET action=movie.php><button class=review_button type=submit name=title value='Top Gun'>Review</button></form></div></div>";
-      ?>
+      $active_filter = $_GET['filter'];
+      $query = 'SELECT name, genres, description FROM movies ORDER BY name';
+      if (isset($active_filter)) {
+        $query = "SELECT name, genres, description FROM movies WHERE genres LIKE '$active_filter' ORDER BY name";
+      }
+      $res = mysqli_query($conn, $query);
+      $movies = mysqli_fetch_all($res, MYSQLI_ASSOC);
+      mysqli_free_result($res);
+      foreach ($movies as $val) { ?>
+        <div class='image' style='background-image: url("<?php echo "./assets/$val[name].jpg" ?>");'>
+          <div class='image_info'>
+            <h3><?php echo ucwords($val['name']) ?></h3>
+            <p><?php echo $val['description'] ?></p>
+            <form method='GET' action='movie.php'>
+              <button class='review_button' type='submit' name='title' value='<?php echo $val['name'] ?>'>Review</button>
+            </form>
+          </div>
+        </div>
+      <?php } ?>
     </div>
   </main>
 </body>
--- src/movie.php
+++ src/movie.php
@@ -1,10 +1,21 @@
-<!DOCTYPE html>
+<?php
+// read filters from database
+$databasename = '127.0.0.1';
+$username = 'root';
+$password = '';
+$conn = new mysqli($databasename, $username, $password, 'ReelRatingsHub');
+
+if (!$conn) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
+
 <html lang="en">
 
 <head>
   <title>
-    <?php $title = $_GET["title"];
-    echo $title; ?></title>
+    <?php $title = $_GET["title"] ?>
+    <?php echo $title ?></title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="css/style.css" rel="stylesheet">
@@ -32,56 +43,62 @@
   </nav>
   <main style="flex-direction: column;">
     <?php
-    $url = './assets/test.jpg'; // temporary until database exists
-    $description = 'Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.';
+    $qr_name = trim(implode(' ', explode('+', $_GET['title'])));
+    $query = "SELECT name, genres, description FROM movies WHERE name LIKE '$qr_name'";
+    $res = mysqli_query($conn, $query);
+    $movie = mysqli_fetch_all($res, MYSQLI_ASSOC);
+    $movie = $movie[0];
+    mysqli_free_result($res);
     $story = 80;
     $characters = 80;
     $originality = 80;
     $emotional_impact = 80;
     $total_score = ($story + $characters + $originality + $emotional_impact) / 4;
-    $comments = array('bożydar' => 'A mi się podoba', 'halinka' => 'Bez sensu, tylko hejtujecie a sami byście lepiej nie zrobili');
-    echo "<div class='movie_review'>";
-    echo "<div class=image style='background-image: url($url);'></div>";
-    echo "<div class=ratings>";
-    echo "<div class=score>"; // oceny wszystkich uśrednione
-    echo "<h2>Other Users reviewed</h2>";
-    echo "<span>Story: $story</span>";
-    echo "<span>Characters: $characters</span>";
-    echo "<span>Originality: $originality</span>";
-    echo "<span>Emotional Impact: $emotional_impact</span>";
-    echo "<span>Total score: $total_score</span>";
-    echo "</div>";
-    echo "<p>$description"; // opis filmu
-    echo "<h2>Your review</h2>";
-    echo "<form class=scoring method=POST action=movie.php>";
-    echo "<label for=story>Story</label>";
-    echo "<input type=number min=0 max=100 name=story placeholder=Story>";
-    echo "<label for=story>Characters</label>";
-    echo "<input type=number min=0 max=100 name=characters placeholder=Characters>";
-    echo "<label for=story>Originality</label>";
-    echo "<input type=number min=0 max=100 name=originality placeholder=Originality>";
-    echo "<label for=story>Emotional Impact</label>";
-    echo "<input type=number min=0 max=100 name=emotional_impact placeholder='Emotional Impact'>";
-    // echo "<h3>Total score: 1</h3>";
-    echo "<button type=submit>Prześlij</button>";
-    echo "</form>";
-    echo "</div>";
-    echo "<div class=comments>";
-    echo "<form class=comment_form method=POST action=movie.php>";
-    echo "<label for=comment><h2>Write your thoughts</h2></label>";
-    echo "<textarea name=comment placeholder='What did you like/dislike about the movie?'>";
-    echo "</textarea>";
-    echo "<button type=submit>Prześlij</button>";
-    echo "</form>";
-    foreach ($comments as $key => $value) {
-      echo "<div class=comment>";
-      echo "<span><h4>$key</h4></span>";
-      echo "<p>$value";
-      echo "</div>";
-    }
-    echo "</div>";
-    echo "</div>";
-    ?>
+    $comments = array('bożydar' => 'A mi się podoba', 'halinka' => 'Bez sensu, tylko hejtujecie a sami byście lepiej nie zrobili'); ?>
+    <div class='movie_review'>
+      <div class='image' style='background-image: url("<?php echo "./assets/$movie[name].jpg" ?>");'></div>
+      <div class='ratings'>
+        <div class='score'>
+          <h2>Other Users reviewed</h2>
+          <span>Story: <?php echo $story ?></span>
+          <span>Characters: <?php echo $characters ?></span>
+          <span>Originality: <?php echo $originality ?></span>
+          <span>Emotional Impact: <?php echo $emotional_impact ?></span>
+          <span>Total score: <?php echo $total_score ?></span>
+        </div>
+        <p><?php echo $movie['description'] ?></p>
+        <h2>Your review</h2>
+        <form class='scoring' method='POST' action='movie.php'>
+          <label for='story'>Story</label>
+          <input type='number' min='0' max='100' name='story' placeholder='Story'>
+          <label for='characters'>Characters</label>
+          <input type='number' min='0' max='100' name='characters' placeholder='Characters'>
+          <label for='originality'>Originality</label>
+          <input type=number min='0' max='100' name='originality' placeholder='Originality'>
+          <label for='emotional_impact'>Emotional Impact</label>
+          <input type='number' min='0' max='100' name='emotional_impact' placeholder='Emotional Impact'>
+          <button type='submit'>Prześlij</button>
+        </form>
+      </div>
+      <div class='comments'>
+        <form class='comment_form' method='POST' action='movie.php'>
+          <label for='comment'>
+            <h2>Write your thoughts</h2>
+          </label>
+          <textarea name='comment' placeholder="What did you like/dislike about the movie?">
+    </textarea>
+          <button type='submit'>Prześlij</button>
+        </form>
+        <?php foreach ($comments as $key => $value) { ?>
+          <div class='comment'>
+            <span>
+              <h4><?php echo $key ?></h4>
+            </span>
+            <p><?php echo $value ?></p>
+          </div>
+        <?php } ?>
+      </div>
+    </div>
   </main>
 </body>
 
